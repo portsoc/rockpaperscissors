5e18c7c2e173eec647f977aca8dac4ed38322d03
ear1grey <rjb@port.ac.uk>
2017-02-17 12:18:31 +0000

    🏆

--- game.js
+++ game.js
@@ -1,31 +1,31 @@
 "use strict";
 
 const results = {
-    "✊": {
-        "✊": "Nobody",
-        "✋": "🤖",
-        "✌": "😀",
-    },
-    "✋": {
-        "✊": "😀",
-        "✋": "Nobody",
-        "✌": "🤖",
-    },
-    "✌": {
-        "✊": "😀",
-        "✋": "🤖",
-        "✌": "Nobody",
-    }
+  "✊": {
+    "✊": "Nobody",
+    "✋": "🤖",
+    "✌": "😀",
+  },
+  "✋": {
+    "✊": "😀",
+    "✋": "Nobody",
+    "✌": "🤖",
+  },
+  "✌": {
+    "✊": "😀",
+    "✋": "🤖",
+    "✌": "Nobody",
+  }
 };
 
 const choice = Object.keys(results);
 
 function respond(e) {
-    let random = Math.floor(Math.random() * choice.length);
-    let computerChoice = choice[random];
-    let humanChoice = e.target.textContent;
-    let winner = results[humanChoice][computerChoice];
-    window.choice.innerHTML = `
+  let random = Math.floor(Math.random() * choice.length);
+  let computerChoice = choice[random];
+  let humanChoice = e.target.textContent;
+  let winner = results[humanChoice][computerChoice];
+  window.choice.innerHTML = `
     <p>😀 chose ${humanChoice}</p>
     <p>🤖 chose ${computerChoice}</p>
     <p>${winner} won!</p>
@@ -33,12 +33,12 @@ function respond(e) {
 }
 
 function setup(say) {
-    let d = document.createElement("button");
-    d.textContent = say;
-    d.onclick = respond;
-    window.game.appendChild(d);
+  let d = document.createElement("button");
+  d.textContent = say;
+  d.onclick = respond;
+  window.game.appendChild(d);
 }
 
 window.addEventListener(
-    "load", () => choice.forEach(setup)
+  "load", () => choice.forEach(setup)
 );
